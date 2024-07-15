8d5fbd2e3762876725fe2b540473208655770fd3
jugesh raghav <74092377+jugeshraghav@users.noreply.github.com>
2024-07-15 20:06:13 +0530

    refractor

--- src/utils/make2DArray.js
+++
@@ -1,7 +0,0 @@
-export function make2DArray(rows, cols) {
-  let array = new Array(rows);
-  for (let i = 0; i < array.length; i++) {
-    array[i] = new Array(cols).fill(0);
-  }
-  return array;
-}
--- src/utils/randomizeArray.js
+++
@@ -1,9 +0,0 @@
-export function randomizeArray(arr, rows, cols) {
-  let res = arr;
-  for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < cols; j++) {
-      arr[i][j] = Math.random(2);
-    }
-  }
-  return res;
-}
