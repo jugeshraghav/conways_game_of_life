6b57572a3b941266cbd0120cd2a932e9826ccbb7
jugesh raghav <74092377+jugeshraghav@users.noreply.github.com>
2024-07-15 12:49:20 +0530

    refractor

--- src/App.jsx
+++ src/App.jsx
@@ -2,98 +2,103 @@ import { useEffect, useState } from "react";
 
 import "./App.css";
 
-function App() {
-  let columns = 30;
-  let rows = 30;
-  let array = [];
-  let futureArray = [];
-  for (let i = 0; i < rows; i++) {
-    array[i] = [];
-    futureArray[i] = [];
+function make2DArray(rows, cols) {
+  let array = new Array(rows);
+  for (let i = 0; i < array.length; i++) {
+    array[i] = new Array(cols);
+  }
+  return array;
+}
 
-    for (let j = 0; j < columns; j++) {
-      array[i][j] = { isLive: false };
-      futureArray[i][j] = { isLive: false };
+function fillArray(arr, rows, cols) {
+  let res = arr;
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      arr[i][j] = 0;
     }
   }
+  return res;
+}
+let columns = 5;
+let rows = 5;
+let array = make2DArray(rows, columns);
+let futureArray = make2DArray(rows, columns);
+array = fillArray(array, rows, columns);
+futureArray = fillArray(array, rows, columns);
+
+const toggleCellHandler = (row_index, column_index) => {
+  let updatedArray = [...boardArray];
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < columns; j++) {
+      if (i === row_index && j === column_index) {
+        updatedArray[i][j] = boardArray[i][j] === 1 ? 0 : 1;
+      }
+    }
+  }
+  setBoardArray([...updatedArray]);
+};
+
+const countNeighbours = (grid, x, y) => {
+  let count = 0;
+  const iStart = x === 0 ? x : x - 1;
+  const jStart = y === 0 ? y : y - 1;
+  const iTill = x === rows - 1 ? x + 1 : x + 2;
+  const jTill = y === columns - 1 ? y + 1 : y + 2;
+  for (let i = iStart; i < iTill; i++) {
+    for (let j = jStart; j < jTill; j++) {
+      if (i === x && j === y) {
+        count = count;
+      } else {
+        count += grid[i][j];
+      }
+    }
+  }
+  console.log(x, y);
+  console.log(count);
+  return count;
+};
+
+const automateCells = () => {
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < columns; j++) {
+      let currentCell = boardArray[i][j];
+
+      let noOfNeighboursAlive = countNeighbours(boardArray, i, j);
+
+      if (currentCell === 1 && noOfNeighboursAlive < 2) {
+        futureArray[i][j] = 0;
+      } else if (currentCell === 1 && noOfNeighboursAlive > 3) {
+        futureArray[i][j] = 0;
+      } else if (currentCell === 0 && noOfNeighboursAlive === 3) {
+        futureArray[i][j] = 1;
+      } else {
+        futureArray[i][j] = boardArray[i][j];
+      }
+    }
+  }
+  setBoardArray([...futureArray]);
+};
+
+let timer;
+const startTheGame = () => {
+  futureArray[1][2] = 1;
+  futureArray[1][1] = 1;
+  futureArray[1][3] = 1;
+  setBoardArray([...futureArray]);
+  // timer = setInterval(() => {
+  //   automateCells();
+  // }, 5000);
+};
+// clearInterval(timer);
+
+const stopTheGame = () => {
+  clearInterval(timer);
+  setIsStarted(false);
+};
+
+function App() {
   const [boardArray, setBoardArray] = useState(array);
   const [isStarted, setIsStarted] = useState(false);
-
-  const toggleCellHandler = (row, column) => {
-    setBoardArray(
-      boardArray.map((r) =>
-        r.map((c) =>
-          r === row && c === column ? { isLive: !c.isLive } : { ...c }
-        )
-      )
-    );
-  };
-
-  const countNeighbours = (grid, x, y) => {
-    let count = 0;
-    const iStart = x === 0 ? x : x - 1;
-    const jStart = y === 0 ? y : y - 1;
-    const iTill = x === rows - 1 ? x + 1 : x + 2;
-    const jTill = y === columns - 1 ? y + 1 : y + 2;
-    for (let i = iStart; i < iTill; i++) {
-      for (let j = jStart; j < jTill; j++) {
-        if (i === x && j === y) {
-          count = count;
-        } else {
-          count = grid[i][j].isLive ? count + 1 : count;
-        }
-      }
-    }
-    return count;
-  };
-
-  const automateCells = () => {
-    for (let i = 0; i < rows; i++) {
-      for (let j = 0; j < columns; j++) {
-        let currentCell = boardArray[i][j];
-
-        let noOfNeighboursAlive = countNeighbours(boardArray, i, j);
-
-        if (currentCell.isLive && noOfNeighboursAlive < 2) {
-          futureArray[i][j].isLive = false;
-        } else if (currentCell.isLive && noOfNeighboursAlive > 3) {
-          futureArray[i][j].isLive = false;
-        } else if (currentCell.isLive === false && noOfNeighboursAlive === 3) {
-          futureArray[i][j].isLive = true;
-        } else {
-          futureArray[i][j] = boardArray[i][j];
-        }
-      }
-    }
-    console.log(futureArray);
-    setBoardArray([...futureArray]);
-  };
-  let timer;
-  const startTheGame = () => {
-    const updatedArray = [...boardArray];
-    (updatedArray[2][1].isLive = true),
-      (updatedArray[2][2].isLive = true),
-      (updatedArray[2][3].isLive = true),
-      setBoardArray(updatedArray);
-    timer = setInterval(() => {
-      automateCells();
-    }, 5000);
-  };
-  clearInterval(timer);
-  const stopTheGame = () => {
-    clearInterval(timer);
-    setIsStarted(false);
-  };
-  // useEffect(() => {
-  //   setBoardArray(array);
-  // }, []);
-
-  // useEffect(() => {
-  //   return () => {
-  //     clearInterval(timer);
-  //   };
-  // }, []);
-  console.log("rerenderd");
   return (
     <>
       {isStarted ? (
@@ -112,14 +117,14 @@ function App() {
       <table>
         <tbody>
           {boardArray.length > 0 &&
-            boardArray.map((row, index) => (
-              <tr key={index}>
+            boardArray.map((row, row_index) => (
+              <tr key={row_index}>
                 {row.length > 0 &&
-                  row.map((column, index) => (
+                  row.map((column, column_index) => (
                     <td
-                      key={index}
-                      className={column.isLive ? "live-cell" : "dead-cell"}
-                      onClick={() => toggleCellHandler(row, column)}
+                      key={column_index}
+                      className={column === 1 ? "live-cell" : "dead-cell"}
+                      onClick={() => toggleCellHandler(row_index, column_index)}
                     ></td>
                   ))}
               </tr>
